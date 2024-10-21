d1d0e4714cf410ae16de63b64d09b344d8ac8865
juliettehnr <juliette.hnr.pro@gmail.com>
2024-10-21 20:32:39 +0200

    tentative exo1 bash

--- Exercice/comptes.sh
+++ Exercice/comptes.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+#Je n'ai pas réussi à faire les exercices bash. Je suis restée bloquée des heures sur le premier exercice. Voici la version finale de mon script et il ne marche pas.
 
 FICHIER=(cat ~/Documents/Projet encadré - Programmation/Exercice1/*/*.ann)
 LOCATION=$(egrep "Location")
