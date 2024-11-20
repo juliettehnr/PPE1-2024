434e4097f45a5060398616c527c0a9847f9ee36a
juliettehnr <juliette.hnr.pro@gmail.com>
2024-11-20 09:44:06 +0100

    update

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -9,23 +9,39 @@ fi
 fichier_url=$1
 num_ligne=1
 
-echo "<html><head><meta></meta></head><body><table><tr><th>N° de ligne</th><th>URL</th><th>Type d'encodage</th><th>Nombre de mots</th></tr>" > tableau_fr.html
+echo "<html>
+<head>
+<meta>charset='UTF-8'</meta>
+</head>
+<body>
+<table>
+<thead>
+<tr>
+<th>N° de ligne</th>
+<th>URL</th>
+<th>Type d'encodage</th>
+<th>Nombre de mots</th>
+</tr>
+</thead>
+<tbody>" > tableau_fr.html
 
 while read -r line
 do
-	http_code=$(curl -s -I -L -w "%{http_code}" -0 /dev/nul $line)
+	http_code=$(curl -s -I -L -w "%{http_code}" -0 /dev/null $line)
  	encoding=$(curl -s -I -L -w "%{content_type}" -0 /dev/null | grep -P -o "charset=\S+" | cut -d'=' -f2) 
   
- 	if [ -z $encoding ]
+ 	if [ -z "$encoding" ]
  	then
 	encoding="N/A"
 	fi 
 
-	nbmots=$(lynx -dwp -nolist $line | wc -w)
-	echo "<tr><td>$num_ligne</td><td>$line</td><td>$encoding</td><td>$nbmots</td>" >> tableau_fr.html
-	num_ligne=$(expr $num_ligne+1)
+	nbmots=$(lynx "$line" -dump -nolist | wc -w)
+	echo -e "<tr><td>$num_ligne</td><td>$line</td><td>$encoding</td><td>$nbmots</td></tr>" >> tableau_fr.html
+	num_ligne=$(expr $num_ligne + 1)
 
 done < $fichier_url
 
-echo "</tr></table></body></html>" >>tableau_fr.html
-
+echo "</tbody>
+</table>
+</body>
+</html>" >> tableau_fr.html
